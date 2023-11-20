3da09bd642882b7f84affafe99117406070a4d3b
vaniafed <ztankist@gmail.com>
2023-11-20 16:45:52 +0300

    Feature app: setting up routes

---
+++ src/app/quiz/[id]/edit/page.tsx
@@ -0,0 +1,3 @@
+export default function QuizEditPage() {
+  return <div>edit</div>;
+}
---
+++ src/app/quiz/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function QuizPassingPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  return <div>passing quiz with id: {params.id}</div>;
+}
