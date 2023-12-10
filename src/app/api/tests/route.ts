68b37d61ed2b194428df5c37e2b0fe0d0d86b619
vaniafed <ztankist@gmail.com>
2023-12-10 16:15:03 +0300

    Feat: mock api

--- src/app/api/tests/route.ts
+++ src/app/api/tests/route.ts
@@ -4,45 +4,44 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
-  const cookie = cookies().get("_session_id");
+  // const cookie = cookies().get("_session_id");
 
-  const response = await axios.get(`${process.env.SNP_URL}/tests`, {
-    headers: {
-      "scope-key": "Rm36-GQ.Z(%rFfwAu:LvY7",
-      Cookie: cookie ? `${cookie.name}=${cookie.value}` : "",
-    },
-  });
-
-  const { data, status } = response;
-  console.log(data);
-
-  return NextResponse.json({ data }, { status });
-}
-
-export async function POST(req: NextRequest) {
-  const test: Pick<Test, "title"> = await req.json();
-
-  const cookie = cookies().get("_session_id");
-
-  const response = await axios.post(`${process.env.SNP_URL}/tests`, test, {
-    headers: {
-      "scope-key": "Rm36-GQ.Z(%rFfwAu:LvY7",
-      Cookie: cookie ? `${cookie.name}=${cookie.value}` : "",
-    },
-  });
-
-  const { data, status } = response;
-
-  // мокнутый API
-  // return NextResponse.json(
-  //   {
-  //     tests: [],
-  //     meta: {
-  //       total_pages: 0,
-  //       total_count: 0,
-  //     },
+  // const response = await axios.get(`${process.env.SNP_URL}/tests`, {
+  //   headers: {
+  //     "scope-key": "Rm36-GQ.Z(%rFfwAu:LvY7",
+  //     Cookie: cookie ? `${cookie.name}=${cookie.value}` : "",
   //   },
-  //   { status: 200 }
-  // );
-  return NextResponse.json({ data }, { status });
+  // });
+
+  // const { data, status } = response;
+  // console.log(data);
+
+  // return NextResponse.json({ data }, { status });
+  return NextResponse.json(
+    {
+      tests: [],
+      meta: {
+        total_pages: 0,
+        total_count: 0,
+      },
+    },
+    { status: 200 }
+  );
 }
+
+// export async function POST(req: NextRequest) {
+//   const test: Pick<Test, "title"> = await req.json();
+
+//   const cookie = cookies().get("_session_id");
+
+//   const response = await axios.post(`${process.env.SNP_URL}/tests`, test, {
+//     headers: {
+//       "scope-key": "Rm36-GQ.Z(%rFfwAu:LvY7",
+//       Cookie: cookie ? `${cookie.name}=${cookie.value}` : "",
+//     },
+//   });
+
+//   const { data, status } = response;
+
+//   return NextResponse.json({ data }, { status });
+// }
---
+++ src/app/api/users/current/route.ts
@@ -0,0 +1,24 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { axios } from "@/lib/axios";
+
+export async function GET() {
+  //   const cookie = cookies().get("_session_id");
+
+  //   const response = await axios.get(`${process.env.SNP_URL}/users/current`, {
+  //     headers: {
+  //       "scope-key": "Rm36-GQ.Z(%rFfwAu:LvY7",
+  //       Cookie: cookie ? `${cookie.name}=${cookie.value}` : "",
+  //     },
+  //   });
+
+  //   const { data, status } = response;
+  //   console.log(data);
+
+  //   return NextResponse.json({ data }, { status });
+
+  return NextResponse.json({
+    username: "vaniafed",
+    is_admin: true,
+  });
+}
