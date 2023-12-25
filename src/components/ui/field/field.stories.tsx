f8bc6d4c81c9acb91e94376779d5f7ac80ee8543
vaniafed <ztankist@gmail.com>
2023-12-26 00:18:11 +0300

    Fix storybook: field

--- src/components/ui/field/field.stories.tsx
+++ src/components/ui/field/field.stories.tsx
@@ -1,57 +1,54 @@
-import React from 'react';
-
+import { Field } from './field';
 import { Input } from '../input';
 import { Textarea } from '../textarea';
-
-import { Field } from './field';
-
-import type { ComponentStory, Meta } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import type { Props } from './props';
 
-const meta: Meta = {
+const meta: Meta<typeof Field> = {
 	title: 'Field',
 	component: Field,
 };
 
+type Story = StoryObj<typeof Field>;
+
+export const Default: Story = {
+	render: () => (
+		<Field id="0">
+			<Input />
+		</Field>
+	),
+};
+
+export const WithLabel: Story = {
+	render: () => (
+		<Field id="1" label="With Label">
+			<Input />
+		</Field>
+	),
+};
+
+export const Required: Story = {
+	render: () => (
+		<Field id="2" label="With Label" required>
+			<Input />
+		</Field>
+	),
+};
+
+export const WithErrMessage: Story = {
+	render: () => (
+		<Field id="2" label="With Label" required errMessage="Typical form error">
+			<Input />
+		</Field>
+	),
+};
+
+export const WithFieldTextArea: Story = {
+	render: () => (
+		<Field id="2" label="With Label">
+			<Textarea />
+		</Field>
+	),
+};
+
 export default meta;
-
-export const Default: ComponentStory<typeof Field> = (args: Props) => <Field {...args} />;
-Default.args = {
-	className: '',
-	children: <Input />,
-};
-
-export const WithLabel: ComponentStory<typeof Field> = (args: Props) => <Field {...args} />;
-WithLabel.args = {
-	className: '',
-	label: 'With Label',
-	id: 'withLabel',
-	children: <Input />,
-};
-
-export const Required: ComponentStory<typeof Field> = (args: Props) => <Field {...args} />;
-Required.args = {
-	className: '',
-	label: 'With Label',
-	id: 'withLabel',
-	required: true,
-	children: <Input />,
-};
-
-export const WithErrMessage: ComponentStory<typeof Field> = (args: Props) => <Field {...args} />;
-WithErrMessage.args = {
-	className: '',
-	label: 'With Label',
-	id: 'withLabel',
-	required: true,
-	errMessage: 'Typical form error',
-	children: <Input />,
-};
-
-export const FieldTextArea: ComponentStory<typeof Field> = (args: Props) => <Field {...args} />;
-FieldTextArea.args = {
-	className: '',
-	label: 'With Label',
-	id: 'withLabel',
-	children: <Textarea />,
-};
