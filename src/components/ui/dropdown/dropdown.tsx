459d7c366823fcbf26e6bfd4507874b3fd340a1f
vaniafed <ztankist@gmail.com>
2023-12-26 00:54:06 +0300

    Fix dropdown: use Chevron icon instead of Image/next

--- src/components/ui/dropdown/dropdown.tsx
+++ src/components/ui/dropdown/dropdown.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Image from 'next/image';
 import classNames from 'classnames/bind';
 import { Input } from '@/components/ui/input';
 import { useDropdown } from '@/hooks/use-dropdown';
 import { DropdownList } from './dropdown-list/';
+import { Chevron } from '@/components/ui/icons/chevron';
 import styles from './dropdown.module.scss';
 import type { FC } from 'react';
 import type { Props } from './props';
@@ -37,7 +37,7 @@ export const Dropdown: FC<Props> = ({
 					readOnly
 					onBlur={onBlur}
 				/>
-				<Image width={18} height={18} src="/ic-dropdown.svg" alt="open" className={cx('dropdown__chevron')} />
+				<Chevron className={cx('dropdown__chevron')} />
 			</div>
 			{isOpened && <DropdownList items={items} active={active} onClick={handleItemClick} ref={menuRef} />}
 		</div>
