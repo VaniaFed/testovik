470e0a812c64f8c59e3bffbb355d21af805d70ff
vaniafed <ztankist@gmail.com>
2023-12-27 02:52:55 +0300

    Fix modal: remove scroll when open modal

--- src/hooks/use-modal.ts
+++ src/hooks/use-modal.ts
@@ -11,12 +11,10 @@ export function useModal(shownByDefault: boolean = false): UseModal {
 
 	const showModal = (): void => {
 		setIsModalShown(true);
-		document.body.setAttribute('style', 'position: fixed; left: 50%; transform: translateX(-50%); width: 100%;');
 	};
 
 	const hideModal = (): void => {
 		setIsModalShown(false);
-		document.body.setAttribute('style', '');
 	};
 
 	return {
