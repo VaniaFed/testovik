868211127e0ff817ab7974e03b2cc2469ce92358
vaniafed <ztankist@gmail.com>
2023-12-27 02:56:11 +0300

    Feat modal-question: validation for multiple type question

--- src/components/ui/modal/modal-question/validation.ts
+++ src/components/ui/modal/modal-question/validation.ts
@@ -2,22 +2,32 @@ import type { FormFields } from '@/components/ui/modal/modal-question/modal-ques
 import type { QuestionType } from '@/reduxjs/modules/tests/types';
 import type { ModalMode } from '@/types/common';
 
-export const getValidationMessage = (formData: FormFields, questionType: QuestionType, mode: ModalMode) => {
+export const getValidationMessage = ({ answers }: FormFields, questionType: QuestionType, mode: ModalMode) => {
 	let errorMessage = '';
 
-	if ((questionType !== 'number' && !formData.answers) || !formData.answers?.length) {
+	if ((questionType !== 'number' && !answers) || !answers?.length) {
 		return null;
 	}
 
-	const correctAnswers = formData.answers.filter((answer) => answer.is_right);
+	const correctAnswers = answers.filter((answer) => answer.is_right);
 
-	if (questionType === 'single') {
-		if (formData.answers.length < 2) {
-			errorMessage = 'Вопрос не может содержать менее 2 вариантов ответа';
-		} else if (correctAnswers.length >= 2) {
-			errorMessage = 'У вопроса не может быть 2 и более правильных вариантов ответа';
+	if (questionType === 'single' || questionType === 'multiple') {
+		if (answers.length < 2) {
+			errorMessage = 'Вопрос не может содержать менее 2-х вариантов ответа';
 		} else if (correctAnswers.length === 0) {
 			errorMessage = 'Вопрос не может не иметь правильных вариантов ответа';
+		}
+	}
+
+	if (questionType === 'single') {
+		if (correctAnswers.length >= 2) {
+			errorMessage = 'У вопроса не может быть 2 и более правильных вариантов ответа';
+		}
+	}
+
+	if (questionType === 'multiple') {
+		if (correctAnswers.length < 2) {
+			errorMessage = 'У вопроса не может быть меньше 2-х правильных вариантов ответа';
 		}
 	}
 
