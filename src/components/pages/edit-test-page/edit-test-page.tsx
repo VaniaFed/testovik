c17bb0b35d4fa3e4ee6f80f2e29ff80b638e6a54
vaniafed <ztankist@gmail.com>
2024-02-12 17:43:40 +0300

    Refactor edit-test-page: extract logic into hook

--- src/components/pages/edit-test-page/edit-test-page.tsx
+++ src/components/pages/edit-test-page/edit-test-page.tsx
@@ -1,100 +1,38 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import classNames from 'classnames/bind';
-import { useAppDispatch, useAppSelector } from '@/reduxjs/hooks';
-import { fetchTestById, deleteQuestion, selectCurrentTest, deleteTest, updateTest } from '@/reduxjs/modules/tests';
 import { Panel } from '@/components/ui/panel';
 import { Button } from '@/components/ui/button';
 import { questionTypeDropdownItems } from '@/utils/data';
-import { useRouter } from 'next/navigation';
 import { AddQuestion } from '@/components/pages/edit-test-page/add-question';
 import { QuestionList } from '@/components/pages/edit-test-page/question-list';
 import { Loader } from '@/components/ui/loader';
 import { Modals } from '@/components/pages/edit-test-page/modals';
-import { EditTestModals } from '@/components/pages/edit-test-page/modals/modals';
 import { TestPageHeader } from '@/components/pages/edit-test-page/test-page-header';
+import { useEditTest } from '@/components/pages/edit-test-page/use-edit-test';
 import styles from './edit-test-page.module.scss';
-import type { ChangeEvent, FC } from 'react';
-import type { Question as IQuestion } from '@/reduxjs/modules/tests';
+import type { FC } from 'react';
 import type { Props } from './props';
-import type { ModalMode } from '@/types/common';
 
 const cx = classNames.bind(styles);
 
 export const EditTestPage: FC<Props> = ({ params: { id }, className }) => {
-	const test = useAppSelector(selectCurrentTest);
-	const [mode, setMode] = useState<ModalMode>('create');
-	const [question, setQuestion] = useState<IQuestion | undefined>(undefined);
-	const [questionType, setQuestionType] = useState(questionTypeDropdownItems[0]);
-	const [title, setTitle] = useState('');
-	const router = useRouter();
-
-	const modalsRef = useRef<EditTestModals>(null);
-
-	const handleAddQuestion = () => {
-		setMode('create');
-		setQuestion(undefined);
-		modalsRef.current?.showEditQuestionModal();
-	};
-
-	const handleEditQuestion = (question: IQuestion) => {
-		setMode('edit');
-		setQuestion(question);
-		modalsRef.current?.showEditQuestionModal();
-	};
-
-	const handleShowDeleteQuestionModal = (question: IQuestion) => {
-		setQuestion(question);
-
-		modalsRef.current?.showDeleteQuestionModal();
-	};
-
-	const handleChangeTitle = (e: ChangeEvent) => {
-		setTitle(e.currentTarget.innerHTML.trim());
-	};
-
-	const handleClickSaveTest = () => {
-		if (test?.title === title) {
-			router.push('/');
-		} else {
-			modalsRef.current?.showSaveTestModal();
-		}
-	};
-
-	const dispatch = useAppDispatch();
-
-	const handleSaveTest = () => {
-		if (test) {
-			dispatch(updateTest({ id: test.id, title }));
-			router.push('/');
-		}
-	};
-
-	const handleDeleteTest = () => {
-		if (test) {
-			dispatch(deleteTest({ id: test.id }));
-			router.push('/');
-		}
-	};
-
-	const handleDeleteQuestion = () => {
-		if (question) {
-			dispatch(deleteQuestion({ id: question.id }));
-
-			modalsRef.current?.hideDeleteQuestionModal();
-		}
-	};
-
-	useEffect(() => {
-		if (id) {
-			dispatch(fetchTestById({ id: Number(id) }));
-		}
-	}, [dispatch, id]);
-
-	useEffect(() => {
-		if (test) {
-			setTitle(test.title);
-		}
-	}, [test]);
+	const {
+		mode,
+		title,
+		test,
+		question,
+		modalsRef,
+		questionType,
+		setQuestionType,
+		handleAddQuestion,
+		handleEditQuestion,
+		handleDeleteQuestion,
+		handleShowDeleteQuestionModal,
+		handleChangeTitle,
+		handleClickSaveTest,
+		handleSaveTest,
+		handleDeleteTest,
+	} = useEditTest(id);
 
 	return (
 		<div className={cx('edit-test-page', className)}>
---
+++ src/components/pages/edit-test-page/use-edit-test.ts
@@ -0,0 +1,109 @@
+import { EditTestModals } from '@/components/pages/edit-test-page/modals/modals';
+import { useAppDispatch, useAppSelector } from '@/reduxjs/hooks';
+import {
+	Question,
+	deleteQuestion,
+	deleteTest,
+	fetchTestById,
+	selectCurrentTest,
+	updateTest,
+} from '@/reduxjs/modules/tests';
+import { ModalMode } from '@/types/common';
+import { questionTypeDropdownItems } from '@/utils/data';
+import { useRouter } from 'next/router';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
+
+export const useEditTest = (testId: string) => {
+	const test = useAppSelector(selectCurrentTest);
+	const [mode, setMode] = useState<ModalMode>('create');
+	const [question, setQuestion] = useState<Question | undefined>(undefined);
+	const [questionType, setQuestionType] = useState(questionTypeDropdownItems[0]);
+	const [title, setTitle] = useState('');
+	const router = useRouter();
+
+	const modalsRef = useRef<EditTestModals>(null);
+
+	const handleAddQuestion = () => {
+		setMode('create');
+		setQuestion(undefined);
+		modalsRef.current?.showEditQuestionModal();
+	};
+
+	const handleEditQuestion = (question: Question) => {
+		setMode('edit');
+		setQuestion(question);
+		modalsRef.current?.showEditQuestionModal();
+	};
+
+	const handleShowDeleteQuestionModal = (question: Question) => {
+		setQuestion(question);
+
+		modalsRef.current?.showDeleteQuestionModal();
+	};
+
+	const handleChangeTitle = (e: ChangeEvent) => {
+		setTitle(e.currentTarget.innerHTML.trim());
+	};
+
+	const handleClickSaveTest = () => {
+		if (test?.title === title) {
+			router.push('/');
+		} else {
+			modalsRef.current?.showSaveTestModal();
+		}
+	};
+
+	const dispatch = useAppDispatch();
+
+	const handleSaveTest = () => {
+		if (test) {
+			dispatch(updateTest({ id: test.id, title }));
+			router.push('/');
+		}
+	};
+
+	const handleDeleteTest = () => {
+		if (test) {
+			dispatch(deleteTest({ id: test.id }));
+			router.push('/');
+		}
+	};
+
+	const handleDeleteQuestion = () => {
+		if (question) {
+			dispatch(deleteQuestion({ id: question.id }));
+
+			modalsRef.current?.hideDeleteQuestionModal();
+		}
+	};
+
+	useEffect(() => {
+		if (testId) {
+			dispatch(fetchTestById({ id: Number(testId) }));
+		}
+	}, [dispatch, testId]);
+
+	useEffect(() => {
+		if (test) {
+			setTitle(test.title);
+		}
+	}, [test]);
+
+	return {
+		mode,
+		title,
+		test,
+		question,
+		modalsRef,
+		questionType,
+		setQuestionType,
+		handleAddQuestion,
+		handleEditQuestion,
+		handleDeleteQuestion,
+		handleShowDeleteQuestionModal,
+		handleChangeTitle,
+		handleClickSaveTest,
+		handleSaveTest,
+		handleDeleteTest,
+	};
+};
